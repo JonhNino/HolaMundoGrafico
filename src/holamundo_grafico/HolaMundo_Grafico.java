094a92d1e0735b9a59b8518f1b687fdd0aa69d34
Ferney Niño <Ferney Nio@DESKTOP-91UIOGN>
2022-07-20 23:25:54 -0500

    20/07/2022 Semana1 Sesion2 Mintic 2022 Java Hola Mundo Grafico

---
+++ src/holamundo_grafico/HolaMundo_Grafico.java
@@ -0,0 +1,12 @@
+
+package holamundo_grafico;
+
+import javax.swing.JOptionPane;
+class HolaMundo_Grafico {
+
+  
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null,"Desde la Caja");
+    }
+    
+}
